4f94124574147e2169ef13f2807148ac8bab59b3
unknown <sonali.hcn@gmail.com>
2023-05-03 05:20:53 +0600

    added codes in index.js file to get data from chefs.json file also made some edits in chefs.json file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
   res.send('Server is running')
+});
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
 })
 
 app.listen(port, () => {

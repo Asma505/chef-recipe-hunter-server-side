43b39aed980b74cc244d9ed457c1199b82a3d843
unknown <sonali.hcn@gmail.com>
2023-05-03 07:02:11 +0600

    installed cors and added codes for cors in index.js file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Server is running')
